ea6f3f3a7eebb2272b26af9971888a666adb1186
luandv03 <dinhvanluan2k3@gmail.com>
2024-05-03 15:34:06 +0700

    update main2

--- test.js
+++ test.js
@@ -1,9 +1,3 @@
-console.log("Hello world");
+console.log("Hello world main 2");
 
-console.log("Nguoc thoi gian ve qua khu");
-
-console.log("Thay doi main1 lan 2");
-
-console.log("Thay doi main1 lan 3");
-
-console.log("Thay doi main1 lan 4");
+console.log("Hello world ae");
